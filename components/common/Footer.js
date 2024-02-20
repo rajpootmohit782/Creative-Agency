a130afdec1fadf3b4f452695ae86986bcb41b3c1
mohit <84062871+rajpootmohit782@users.noreply.github.com>
2024-02-20 07:35:00 +0000

    LATEST

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,16 +1,20 @@
-import Link from "next/link"
-import { TitleLogo } from "./Title"
-import { BsFacebook } from "react-icons/bs"
-import { AiFillBehanceCircle, AiFillInstagram, AiFillLinkedin } from "react-icons/ai"
+import Link from "next/link";
+import { TitleLogo } from "./Title";
+import { BsFacebook } from "react-icons/bs";
+import {
+  AiFillBehanceCircle,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className='container'>
-          <div className='grid-4'>
-            <div className='logo'>
-              <TitleLogo title='creative' caption='7' className='logobg' />
+        <div className="container">
+          <div className="grid-4">
+            <div className="logo">
+              <TitleLogo title="creative" caption="7" className="logobg" />
               <br />
               <span>
                 Questions? Reach us <br /> Monday – Friday from 9 am to 6 pm
@@ -19,76 +23,76 @@ const Footer = () => {
               <br />
               <h3>+1 001 234 5678</h3>
               <br />
-              <button className='button-primary'>Request for quote</button>
+              <button className="button-primary">Request for quote</button>
             </div>
             <ul>
               <h3>COMPANY</h3>
               <li>
-                <Link href='/'>About agency</Link>
+                <Link href="/">About agency</Link>
               </li>
               <li>
-                <Link href='/'>Our team</Link>
+                <Link href="/">Our team</Link>
               </li>
               <li>
-                <Link href='/'>Showcase</Link>
+                <Link href="/">Showcase</Link>
               </li>
               <li>
-                <Link href='/'>Blog</Link>
+                <Link href="/">Blog</Link>
               </li>
               <li>
-                <Link href='/'>Demo design system</Link>
+                <Link href="/">Demo design system</Link>
               </li>
               <li>
-                <Link href='/'>Contact</Link>
+                <Link href="/">Contact</Link>
               </li>
             </ul>
             <ul>
               <h3>SERVICES</h3>
               <li>
-                <Link href='/'>Web Design & Development</Link>
+                <Link href="/">Web Design & Development</Link>
               </li>
               <li>
-                <Link href='/'>Branding & Creative Services</Link>
+                <Link href="/">Branding & Creative Services</Link>
               </li>
               <li>
-                <Link href='/'>Digital Marketing</Link>
+                <Link href="/">Digital Marketing</Link>
               </li>
               <li>
-                <Link href='/'>E-Commerce</Link>
+                <Link href="/">E-Commerce</Link>
               </li>
             </ul>
             <ul>
               <h3>CONNECT</h3>
-              <div className='connect'>
+              <div className="connect">
                 <li>
-                  <Link href='/'>
+                  <Link href="/">
                     <BsFacebook size={25} />
                   </Link>
                 </li>
                 <li>
-                  <Link href='/'>
+                  <Link href="/">
                     <AiFillBehanceCircle size={25} />
                   </Link>
                 </li>
                 <li>
-                  <Link href='/'>
+                  <Link href="/">
                     <AiFillInstagram size={25} />
                   </Link>
                 </li>
                 <li>
-                  <Link href='/'>
+                  <Link href="/">
                     <AiFillLinkedin size={25} />
                   </Link>
                 </li>
               </div>
             </ul>
           </div>
-          <div className='legal connect py'>
-            <div className='text'>
-              <span>© 2023 THE SEVEN. ALL RIGHTS RESERVED.</span>
+          <div className="legal connect py">
+            <div className="text">
+              <span>© 2024 THE MOHIT. ALL RIGHTS RESERVED.</span>
             </div>
-            <div className='connect'>
-              <span>GORKCODER COMPANY</span>
+            <div className="connect">
+              <span>MOHIT RAJPOOT</span>
               <span> &nbsp; | &nbsp; </span>
               <span>TERMS & CONDITIONS</span>
             </div>
@@ -96,7 +100,7 @@ const Footer = () => {
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
